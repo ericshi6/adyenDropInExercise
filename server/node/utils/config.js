2cf22c89bfec571d7f9280216ed57c090a50b2cb
ericshi6 <ericshi6@gmail.com>
2021-06-06 18:44:16 -0700

    updated config.js

--- server/node/utils/config.js
+++ server/node/utils/config.js
@@ -2,7 +2,7 @@
 //hard coded env 
 const CHECKOUT_APIKEY='AQEyhmfxK4zJbBZDw0m/n3Q5qf3VaY9UCJ1+XWZe9W27jmlZiniYHPZ+YtXG9dYfNdwN0H8QwV1bDb7kfNy1WIxIIkxgBw==-uA2G0DS73SlmB4EHi/YNndhli7KlCMjXHbMmm8stboc=-djvcdM2gNHq9dSvC';
 const MERCHANT_ACCOUNT='TestAccountNY';
-const CLIENT_KEY='pub.v2.8016227695560099.aHR0cHM6Ly9oZXJva3VhcHAuY29t.paK1jR_qLLIr6CbChr2FaeNZloZHrpslsI7MvPJoOKs'
+const CLIENT_KEY='pub.v2.8016227695560099.aHR0cHM6Ly90cmFucXVpbC10ZW1wbGUtNTUwMjEuaGVyb2t1YXBwLmNvbQ.5HuglU6lcGek1jawBnDRvsT3gSSmYksLZ0ZD5kwzTY0';
 
 const API_VERSION = 'v67';
 const CHECKOUT_URL = `https://checkout-test.adyen.com/${API_VERSION}`;
