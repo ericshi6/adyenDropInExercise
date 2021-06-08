4b786dc8161a68ce33a8692d515b7a06d277f5a7
ericshi6 <ericshi6@gmail.com>
2021-06-08 07:46:09 -0700

    added comments in dropin.js to show workflow/thought process

--- server/node/utils/handleCallback.js
+++ server/node/utils/handleCallback.js
@@ -8,8 +8,8 @@ module.exports = ({ error, response = {}, body }, res) => {
         console.log(`Request to ${res.req.url} ended with status ${response.statusCode} - ${response.statusMessage}`);
         
         //Eric res body
-        console.log(res.req.body);
-        console.log(response.body);
+        //console.log(res.req.body);
+        //console.log(response.body);
     }
 
     res.send(body);
--- src/dropin.js
+++ src/dropin.js
@@ -1,8 +1,15 @@
-// 0. Get clientKey
+//See documentation for directions 
+//https://docs.adyen.com/online-payments/drop-in-web/
+
+// Step 1: Get available payment methods
+
+// 0. Get clientKey - Make an API call to obtain the origin key with the provided API key
+// https://docs.adyen.com/user-management/how-to-get-an-origin-key#generate-multiple-origin-keys
+
 getClientKey().then(clientKey => {
     getPaymentMethods().then(paymentMethodsResponse => {
-        // 1. Create an instance of AdyenCheckout
-        const checkout = new AdyenCheckout({
+
+        const configuration = {
             environment: 'test',
             clientKey: clientKey, // Mandatory. clientKey from Customer Area
             paymentMethodsResponse,
@@ -15,7 +22,10 @@ getClientKey().then(clientKey => {
                 // state.isValid;
                 makePayment(state.data);
             }
-        });
+        }
+        // 1. Create an instance of AdyenCheckout
+        // Use the configuration object to create an instance of AdyenCheckout. Then use the returned value to create and mount the instance of Drop-in. 
+        const checkout = new AdyenCheckout(configuration);
 
         // 2. Create and mount the Component
         const dropin = checkout
