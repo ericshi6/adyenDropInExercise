f7b7522a013ceebdb0ff9d3afd83a567bb8f7986
ericshi6 <ericshi6@gmail.com>
2021-06-08 09:36:07 -0700

    configuration in dropin.js

--- src/dropin.js
+++ src/dropin.js
@@ -8,10 +8,10 @@
 
 getClientKey().then(clientKey => {
     getPaymentMethods().then(paymentMethodsResponse => {
-
+        //Create a configuration object
         const configuration = {
             environment: 'test',
-            clientKey: clientKey, // Mandatory. clientKey from Customer Area
+            clientKey: clientKey, // Mandatory. clientKey from Customer Area. Web Drop-in versions before 3.10.1 use originKey instead of clientKey.
             paymentMethodsResponse,
             removePaymentMethods: ['paysafecard', 'c_cash'],
             onChange: state => {
@@ -20,8 +20,48 @@ getClientKey().then(clientKey => {
             onSubmit: (state, component) => {
                 // state.data;
                 // state.isValid;
-                makePayment(state.data);
-            }
+                // Global configuration for onSubmit
+                // Your function calling your server to make the `/payments` request
+                makePayment(state.data)
+                .then(response => {
+                    if (response.action) {
+                      // Drop-in handles the action object from the /payments response
+                      dropin.handleAction(response.action);
+                    } else {
+                      // Your function to show the final result to the shopper
+                      showFinalResult(response);
+                    }
+                  })
+                  .catch(error => {
+                    throw Error(error);
+                  });
+            },
+            onAdditionalDetails: (state, dropin) => {
+                // Your function calling your server to make a `/payments/details` request
+                makeDetailsCall(state.data)
+                  .then(response => {
+                    if (response.action) {
+                      // Drop-in handles the action object from the /payments response
+                      dropin.handleAction(response.action);
+                    } else {
+                      // Your function to show the final result to the shopper
+                      showFinalResult(response);
+                    }
+                  })
+                  .catch(error => {
+                    throw Error(error);
+                  });
+              },
+              paymentMethodsConfiguration: {
+                card: { // Example optional configuration for Cards
+                  hasHolderName: true,
+                  holderNameRequired: true,
+                  enableStoreDetails: true,
+                  hideCVC: false, // Change this to true to hide the CVC field for stored cards
+                  name: 'Credit or debit card',
+                  onSubmit: () => {}, // onSubmit configuration for card payments. Overrides the global configuration.
+                }
+              }
         }
         // 1. Create an instance of AdyenCheckout
         // Use the configuration object to create an instance of AdyenCheckout. Then use the returned value to create and mount the instance of Drop-in. 
